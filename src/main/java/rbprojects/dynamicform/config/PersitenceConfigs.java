e2e811289cacf74116e72f8f1e589f0ce7c445f3
Rodrigo <rbordinhon@gmail.com>
2016-01-30 13:23:24 -0200

    Ajustes para compilar no maven

--- src/main/java/rbprojects/dynamicform/config/PersitenceConfigs.java
+++ src/main/java/rbprojects/dynamicform/config/PersitenceConfigs.java
@@ -5,7 +5,6 @@ import java.text.MessageFormat;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
